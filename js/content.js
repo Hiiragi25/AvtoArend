93bbcc3fab470cc2147f14e63d506fddf5a6081d
Medvedev Pavel <146516131+Hiiragi25@users.noreply.github.com>
2023-11-06 17:38:39 +0300

    Add files via upload

---
+++ js/content.js
@@ -0,0 +1,537 @@
+
+const RIDE = {
+
+   'section' : [
+      {
+      'title' : 'Аренда в три простых шага',
+      'titlesub': 'Как это работает?'
+      }
+   ],
+
+
+   'content' : [
+     {
+       id: '1',
+       'title' : 'Выберите местоположение',
+       'text'  : 'Выберите в каком городе вы ходите ариндовать авто. На нашем сайте есть большой выбор машин по всей России',
+       'icon'  : 'bxs-map'
+    },
+     {
+       id: '2',
+       'title' : 'Укажите дату получения',
+       'text'  : 'Выберите дату получение выбранного вами авто и доставим ваш автомобиль в любую точку России',
+       'icon'  : 'bxs-calendar-check'
+    },
+     {
+       id: '3',
+       'title' : 'Закажите Автомобиль',
+       'text'  : 'На нашем сайте заказать автомобиль проще простого. Подбор автомобилей происходит по вашим предпочтением',
+       'icon'  : 'bxs-calendar-star'
+    }
+
+
+   ]
+};
+const SERVES = {
+ 
+  'section' : [
+      {
+      'title' : 'Большой выбор авто',
+      'titlesub': 'Аренда'
+      }
+   ],
+
+
+  'content' : [
+
+    {
+   
+       'id':'1',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car1.jpg'
+
+    },
+     {
+   
+       'id':'2',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car2.jpg'
+
+    },
+     {
+   
+       'id':'3',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car3.jpg'
+
+    },
+     {
+   
+       'id':'4',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car4.jpg'
+
+    },
+     {
+   
+       'id':'5',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car5.jpg'
+
+    },
+     {
+   
+       'id':'6',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car6.jpg'
+
+    },
+      {
+   
+       'id':'7',
+       'name':'2018 Honda Civic',
+      'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car1.jpg'
+
+    },
+      {
+   
+       'id':'8',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car2.jpg'
+    },
+     {
+   
+       'id':'9',
+       'name':'2018 Honda Civic',
+       'cena' : '2млн.руб',
+       'month' : '50тыс.руб',
+       'age' : '2017',
+       'img' : 'car3.jpg'
+    },
+
+     
+   ]
+
+}
+const CONTACT = {
+
+ 'section' : [
+    {
+    'title' : 'Контакты',
+    'titlesub': 'Как это работает?'
+    }
+  ],
+
+	'content' : [
+
+	  {
+	 
+	     'title':'Телефон',
+	     'contact':'+878979789G'
+	  },
+	   {
+	 
+	     'title':'Адрес',
+	     'contact':'ул.Победы 35аG'
+	  },
+	   {
+	 
+	     'title':'Почта',
+	     'contact':'avtoarend@gmail.comG'
+	  }
+	 ]
+};
+const ABOUT = {
+  'section' : [
+      {
+      'title' : 'Лучший опыт работы с клиентами',
+      'titlesub': 'О нас'
+      }
+   ],
+
+
+   'content' : [
+     {
+      
+       'title' : 'Наша компнаия',
+       'text'  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eaque possimus consequuntur, ab temporibus fugiat, hic odio deserunt earum, placeat quia? Accusantium dolor sequi sint aliquam modi quas, eveniet nisi!',
+       'img'  : '1684825855_polinka-top-p-avtomobil-na-belom-fone-kartinka-instagram-13.jpg'
+    }
+    
+
+
+   ]
+
+};
+const REVIREWS = {
+ 
+   'section' : [
+    {
+    'title' : 'О нас говорят',
+    'titlesub': 'Отзывы'
+    }
+   ],
+
+
+   'content' : [
+     {
+       id: '1',
+       'name' : 'Анна',
+       'text'  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non placeat, corporis illum quia? Expedita necessitatibus rem praesentium eos enim veritatis. Alias, facilis ipsam culpa laudantium possimus fugiat deleniti molestias?',
+       'img'  : 'rev3.jpg',
+       'data' : '12.03.2023'
+    },
+      {
+       id: '2',
+       'name' : 'Анна',
+       'text'  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non placeat, corporis illum quia? Expedita necessitatibus rem praesentium eos enim veritatis. Alias, facilis ipsam culpa laudantium possimus fugiat deleniti molestias?',
+       'img'  : 'rev3.jpg',
+       'data' : '12.03.2023'
+    },
+       {
+       id: '3',
+       'name' : 'Анна',
+       'text'  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non placeat, corporis illum quia? Expedita necessitatibus rem praesentium eos enim veritatis. Alias, facilis ipsam culpa laudantium possimus fugiat deleniti molestias?',
+       'img'  : 'rev3.jpg',
+       'data' : '12.03.2023'
+    },
+    {
+       id: '4',
+       'name' : 'Анна',
+       'text'  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non placeat, corporis illum quia? Expedita necessitatibus rem praesentium eos enim veritatis. Alias, facilis ipsam culpa laudantium possimus fugiat deleniti molestias?',
+       'img'  : 'rev3.jpg',
+       'data' : '12.03.2023'
+   }
+     
+   
+   ]
+
+};
+
+const QUESTIONS = {
+
+
+ 'section' : [
+    {
+    'title' : 'Ответы на вопросы',
+    'titlesub': 'Q&A'
+    }
+  ],
+  'content' : [
+
+	  {
+	 
+	     'questions':'Как можно оплатить аренду автомобиля ?',
+	     'answer':'Вы можите оплатить аренду на нашем сайты любым удлбным для вас способом интернет оплаты'
+	  },
+	   {
+	     'questions':'Как можно оплатить аренду автомобиля ?',
+	     'answer':'Вы можите оплатить аренду на нашем сайты любым удлбным для вас способом интернет оплаты'
+	    
+	  },
+	   {
+	 
+	     'questions':'Как можно оплатить аренду автомобиля ?',
+	     'answer':'Вы можите оплатить аренду на нашем сайты любым удлбным для вас способом интернет оплаты'
+
+	  }
+	 ]
+};
+
+const ADVERTISEMENT = [
+ {
+  'title':'Получите скидку при аренде своего первого автомобиля!',
+  'img' : '1612851795_171-p-krasnaya-mashina-bez-fona-251.jpg' 
+  } 
+];
+
+GetRide(RIDE);
+GetContact(CONTACT);
+GetServes(SERVES);
+GetAbout(ABOUT);
+GetRewiews(REVIREWS);
+GetQuestionses(QUESTIONS);
+GetAdvertisement(ADVERTISEMENT);
+
+
+
+
+
+function GetRide(RIDE){
+   
+    const rideHeader = document.querySelector('.header__section--ride');
+    const rideConteiner = document.querySelector('.ride__wrapper');
+	        
+	  let  rideHeaderText = `  
+	     
+	        <span class="section__subtitle">${RIDE.section[0].titlesub}</span>
+		 	    <h2 class="section__title">${RIDE.section[0].title}</h2>
+
+	        `;
+
+	       rideHeader.insertAdjacentHTML('beforeend',rideHeaderText);
+    
+     for (let i in RIDE.content){
+
+         let rideBlock = `    
+
+          
+ 	 		    <div class="ride__block">
+	 	 			<i class="bx ${RIDE.content[i].icon}"></i>
+	 	 			<h2 class="ride__block__title">${RIDE.content[i].title}</h2>
+	 	 			<p class="ride__block__text">${RIDE.content[i].text}</p>
+ 	 	
+
+         `;
+         rideConteiner.insertAdjacentHTML('beforeend',rideBlock);
+
+
+   } 
+}   
+
+function GetContact(CONTACT){
+
+	const contactConteiner = document.querySelector('.contact__list');
+  const contactTitle = document.querySelector('.contect__title');
+
+    contactTitle.textContent =  CONTACT.section[0].title;
+
+
+    for (let i in CONTACT.content){
+	        
+	      let contactContent = `  
+	  
+	  				<div class="nav__item">
+	  					<h3>${CONTACT.content[i].title}</h3>
+	  					<p>${CONTACT.content[i].contact}</p>
+	  				</div>
+	  		
+
+	        `;
+
+	        contactConteiner.insertAdjacentHTML('beforeend',contactContent);
+	       
+     }    
+}
+function GetServes(SERVES){
+   
+ const servesConteiner = document.querySelector('.serves__conteiner');
+ const servesHeader = document.querySelector('.section__header--serves');
+
+
+  let servesHeaderText = `  
+
+
+      <span class="section__subtitle">${SERVES.section[0].titlesub}</span>
+      <h2 class="section__title">${SERVES.section[0].title}</h2>
+
+
+  `;
+  servesHeader.insertAdjacentHTML('beforeend',servesHeaderText);
+
+
+ for (let i in SERVES.content){
+          
+      let servesList = `  
+
+          <div class="serves__block">
+                <div class="serves__item">
+                  <div class="serves__icons">
+                  <div class="icons__item">
+                    <i class='bx bx-search-alt-2'>
+                  </i>
+                  </div>
+                 </div>
+                    <img class="serves__img" src="img/${SERVES.content[i].img}">
+                 </div>
+
+                 <div class="serves__content">
+                   <p class="serves__age">${SERVES.content[i].age}</p>
+                   <h2 class="serves__cena">${SERVES.content[i].cena} | ${SERVES.content[i].month} <span class="serves__accent">/ в месяц </span></h2>
+                   <h3 class="serves__name">${SERVES.content[i].name} </h3>
+                   <a href="#" class="serves__link">Rent Now</a>
+                 </div>
+              </div>
+      `;
+       servesConteiner.insertAdjacentHTML('beforeend',servesList);        
+
+
+
+  }
+}
+function GetAbout(ABOUT){
+
+  const aboutConteiner = document.querySelector('.conteiner__about');
+  
+ 
+
+     let aboutContent = `    
+
+     <div class="section__header">
+      <span class="section__subtitle">${ABOUT.section[0].titlesub}</span>
+      <h2 class="section__title">${ABOUT.section[0].title}</h2>
+      </div>
+      </div>
+     <div class="about__conteiner conteiner">
+       <div class="about__block " data-aos="fade-up-right">
+       <img class="about__img" src="img/${ABOUT.content[0].img}">
+     </div>
+     <div class="about__block"  data-aos="fade-up-left">
+       <div class="about__content">
+       <span class="about__title about__title--text"> ${ABOUT.content[0].title}</span>
+        <p class="about__text">
+          ${ABOUT.content[0].text}
+        </p>
+         <input class="about__button" type="submit" value="Подробнее">
+       </div>
+     </div>
+
+     `;
+     aboutConteiner.insertAdjacentHTML('beforeend',aboutContent);
+}
+function GetQuestionses(QUESTIONS){
+
+
+  const  questionsContiner = document.querySelector('.accordion');
+  const  questionsHeader = document.querySelector('.section__header--questions');
+
+  let questionsHeaderText = `   
+
+       
+      <span class="section__subtitle">${QUESTIONS.section[0].titlesub}</span>
+      <h2 class="section__title">${QUESTIONS.section[0].title}</h2>
+  `;
+
+   questionsHeader.insertAdjacentHTML('beforeend',questionsHeaderText);
+
+
+
+
+   for (let i in QUESTIONS.content) {
+
+   let questionsList = `   
+
+
+     <div class="accordion__item">
+	 	 	   	 	<div class="accordion__trigger">
+	 	 	   	   	   <p class="accordion__title">${QUESTIONS.content[i].questions}</p>
+	 	 	   	   	   <i class="bx bx-plus"></i>
+	 	 	   	   </div>
+	 	 	   	   <div class="accordion__content">
+	 	 	   	   	   <p class="accordion__text">${QUESTIONS.content[i].answer}</p>
+	 	 	   	  </div>
+	 	 	</div>
+
+
+
+   `;
+
+
+    questionsContiner.insertAdjacentHTML('beforeend',questionsList);
+   }
+}
+function GetRewiews(REWIEWS){
+     
+     const rewiewsConteiner = document.querySelector('.revirews__wrapper');
+     const rewiewsHeader = document.querySelector('.section__header--reviews');
+
+
+
+     let rewiewsHeaderText = `    
+
+      <span class="section__subtitle">${REWIEWS.section[0].titlesub}</span>
+      <h2 class="section__title">${REWIEWS.section[0].title}</h2>
+     `;
+
+      rewiewsHeader.insertAdjacentHTML('beforeend',rewiewsHeaderText);
+
+
+
+       for( let i in REWIEWS.content ){
+         
+
+        let rewiesList = ` 
+         <div class="reviews__block swiper-slide">
+            <div class="revies__block-header">
+              <div class="reviews__img">
+              <img src="img/${REWIEWS.content[i].img}">
+            </div>
+            <h2>${REWIEWS.content[i].name}</h2>
+            <div class="rewiews__stars">
+              <i class="bx bxs-star"></i>
+              <i class="bx bxs-star"></i>
+              <i class="bx bxs-star"></i>
+              <i class="bx bxs-star"></i>
+              <i class="bx bxs-star-haif"></i>
+            </div>
+            </div>
+            <div class="rewiews__content">
+              <p class="rewiews__text">${REWIEWS.content[i].text}</p>
+            </div>
+          </div>
+
+            `;
+
+            rewiewsConteiner.insertAdjacentHTML('beforeend',rewiesList);
+     
+       }
+}
+function GetAdvertisement(ADVERTISEMENT){
+ 
+ const advertisenentConteiner = document.querySelector('.advertisement__conteiner');
+
+ 
+  let advertisenentContent = `  
+
+    <i class="bx bx-x advertisement__close"></i>
+         <div class="advertisement__content">
+          <h2 class="advertisement__title">${ADVERTISEMENT[0].title}</h2>
+      <div class="advertisement__block">
+        <img class="advertisement__img" src="img/${ADVERTISEMENT[0].img}">
+        </div>
+    <input class="advertisement__btn" type="submit" value="Получить">
+
+  `;
+ advertisenentConteiner.insertAdjacentHTML('beforeend',advertisenentContent);
+}
+
+
+
+
+    
+
+
+
+
+
+
+
+
+  
+
+
+
